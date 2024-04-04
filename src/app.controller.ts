c504a85c0f39bacd47dd285b90015d2a674dd6c5
devPark <devpark@gmail.com>
2024-04-04 14:51:57 +0900

    Dependancy injection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+// 1차 path
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
+  // 2차 path
+  @Get('blue/:id/:name')
+  getHello(
+    @Req() req: Request,
+    @Body() body,
+    @Param param: { id: string; name: string },
+  ): string {
+    console.log(param);
     return this.appService.getHello();
   }
 }
